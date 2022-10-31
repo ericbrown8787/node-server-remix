fbe67afa79c76035d390bfdac031aa4818df51be
Eric Brown <eric.brown8787@gmail.com>
2022-10-31 00:13:17 -0400

    Test obtain and store JSON from github api

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const axios = require('axios');
+const fs = require('fs');
 
 const app = express();
 
@@ -13,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.set('view engine', 'nunjucks');
 
+// ======Render correct pages======
 app.get('/', async (req, res) => {
   res.render('index.njk');
 });
@@ -21,8 +24,46 @@ app.get('/info', async (req, res) => {
   res.render('info.njk');
 });
 
-app.get('/404', async (req, res) => {
-  res.render('index.njk');
+// app.get('/404', async (req, res) => {
+//   res.render('index.njk');
+// });
+
+// ======Get my public github info======
+app.get('/whoami', async (req, res) => {
+  let data = {};
+
+  try {
+    const response = await axios.get('https://api.github.com/users/ericbrown8787');
+    data = response.data;
+  } catch (e) {
+    data = { message: 'User not found' };
+  } finally {
+    data = {
+      ...data,
+      docs: 'https://docs.github.com/rest/reference/users#get-a-user',
+    };
+  }
+  fs.writeFileSync('./local-logs/githubplaintext.txt', JSON.stringify(data));
+  fs.writeFileSync('./local-logs/githubjson.json', JSON.stringify(data));
+  res.send(data);
+});
+
+app.get('/fsc', async (req, res) => {
+  let data = {};
+
+  try {
+    const response = await axios.get('https://api.github.com/users/ericbrown8787');
+    data = response.data;
+  } catch (e) {
+    data = { message: 'User not found' };
+  } finally {
+    data = {
+      ...data,
+      docs: 'https://docs.github.com/rest/reference/users#get-a-user',
+    };
+  }
+
+  res.send(data);
 });
 
 app.use((req, res) => {
